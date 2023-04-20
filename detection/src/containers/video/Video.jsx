6800538985e822b4684af4bc79c62a6624ae2a51
Daniel Currey <spiceycurrey@gmail.com>
2023-04-19 17:24:46 -0700

    Added video

---
+++ detection/src/containers/video/Video.jsx
@@ -0,0 +1,19 @@
+import './header.css';
+
+import React from 'react';
+import particles from '../../assets/ai.png';
+
+const Header = () => (
+  <div className="det__header section__padding" id="home">
+    <div className="det__header-content">
+      <h1 className="gradient__text">A captivating VR experience.</h1>
+      <p>Set in a future reality in 2078, you assume the role of a killer AI robot set on targeted rampages by a mysterious group.</p>
+    </div>
+
+    <div className="det__header-image">
+      <img src={particles} />
+    </div>
+  </div>
+);
+
+export default Header;
